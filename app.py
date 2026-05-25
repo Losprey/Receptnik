ec49a9be0d9f9553e4215fd11cf683ebac339184
Losprey <Waldis994@gmail.com>
2026-05-25 16:12:07 +0200

    AI update

--- app.py
+++ app.py
@@ -28,6 +28,93 @@ def api_recepty():
     recepty = nacitaj()
     return jsonify(recepty)
 
+@app.route('/api/recepty/<int:index>')
+def api_recept_detail(index):
+    recepty = nacitaj()
+    if 0 <= index < len(recepty):
+        return jsonify(recepty[index])
+    return jsonify({"error": "Recept neexistuje"}), 404
+
+@app.route('/api/recepty', methods=['POST'])
+def api_pridat_recept():
+    data = request.get_json()
+    if not data or 'nazov' not in data or 'ingrediencie' not in data or 'postup' not in data:
+        return jsonify({"error": "Chybajú povinné polia (nazov, ingrediencie, postup)"}), 400
+    
+    recepty = nacitaj()
+    novy_recept = {
+        "nazov": data['nazov'],
+        "ingrediencie": data['ingrediencie'],
+        "postup": data['postup'],
+        "kategoria": data.get('kategoria', ''),
+        "favorite": data.get('favorite', False)
+    }
+    recepty.append(novy_recept)
+    uloz(recepty)
+    return jsonify(novy_recept), 201
+
+@app.route('/api/recepty/<int:index>', methods=['PUT'])
+def api_upravit_recept(index):
+    recepty = nacitaj()
+    if 0 <= index < len(recepty):
+        data = request.get_json()
+        if 'nazov' in data:
+            recepty[index]['nazov'] = data['nazov']
+        if 'ingrediencie' in data:
+            recepty[index]['ingrediencie'] = data['ingrediencie']
+        if 'postup' in data:
+            recepty[index]['postup'] = data['postup']
+        if 'kategoria' in data:
+            recepty[index]['kategoria'] = data['kategoria']
+        if 'favorite' in data:
+            recepty[index]['favorite'] = data['favorite']
+        uloz(recepty)
+        return jsonify(recepty[index])
+    return jsonify({"error": "Recept neexistuje"}), 404
+
+@app.route('/api/recepty/<int:index>', methods=['DELETE'])
+def api_zmazat_recept(index):
+    recepty = nacitaj()
+    if 0 <= index < len(recepty):
+        zmazany = recepty.pop(index)
+        uloz(recepty)
+        return jsonify(zmazany)
+    return jsonify({"error": "Recept neexistuje"}), 404
+
+@app.route('/api/kategorie')
+def api_kategorie():
+    recepty = nacitaj()
+    kategorie = set()
+    for r in recepty:
+        if r.get('kategoria'):
+            kategorie.add(r['kategoria'])
+    return jsonify(sorted(list(kategorie)))
+
+@app.route('/api/statistiky')
+def api_statistiky():
+    recepty = nacitaj()
+    pocet = len(recepty)
+    oblubene = len([r for r in recepty if r.get('favorite')])
+    kategorie = {}
+    for r in recepty:
+        kat = r.get('kategoria', 'bez kategorie')
+        kategorie[kat] = kategorie.get(kat, 0) + 1
+    return jsonify({
+        "pocet_receptov": pocet,
+        "pocet_oblubenych": oblubene,
+        "kategorie": kategorie
+    })
+
+@app.route('/api/vyhladat')
+def api_vyhladat():
+    query = request.args.get('q', '').lower()
+    recepty = nacitaj()
+    if query:
+        vysledky = [r for r in recepty if query in r['nazov'].lower() or query in r['ingrediencie'].lower()]
+    else:
+        vysledky = recepty
+    return jsonify(vysledky)
+
 @app.route('/pridat', methods=['POST'])
 def pridat():
     nazov = request.form['nazov']
